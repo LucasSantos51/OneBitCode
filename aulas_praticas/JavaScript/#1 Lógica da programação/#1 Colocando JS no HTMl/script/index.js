b7ad863187c2db4c6141e8a34754bdd893fa54a5
LucasSantos51 <123632958+LucasSantos51@users.noreply.github.com>
2023-03-15 10:28:58 -0300

    Primeiros passos com JavaScript

---
+++ aulas_praticas/JavaScript/#1 Lógica da programação/#1 Colocando JS no HTMl/script/index.js
@@ -0,0 +1,7 @@
+let nome = "Lucas"
+let idade = 17
+let futuraProfissao = "FrontEndDeveloper"
+
+console.log(nome)
+console.log(idade)
+console.log(futuraProfissao)
---
+++ aulas_praticas/JavaScript/#1 Lógica da programação/#2 Recurso nativos do navegador/Script/index.js
@@ -0,0 +1,11 @@
+
+
+let nome = window.prompt("Digite seu nome")
+window.alert("Seja bem-vindo: " + nome) 
+let confirm = window.confirm("Você realmente deseja navegar nessa pagina?")
+
+console.log(nome)
+console.log(confirm)
+
+console.error("Essa pagina não foi configurada completamente")
+console.warn("Recomendamos que você retorne mais tarde")
