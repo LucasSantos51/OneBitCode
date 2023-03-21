8c3aedb62274e264ead1c65a2252f529384308e6
LucasSantos51 <123632958+LucasSantos51@users.noreply.github.com>
2023-03-21 19:30:05 -0300

    Praticando JS

---
+++ aulas_praticas/JavaScript/#1 Lógica da programação/#2.1 Aplicando novamente os conceitos/JavaScript/base.js
@@ -0,0 +1,13 @@
+alert("Seja bem-vindo \n" + "À seguir faremos um breve cadastro:")
+
+const nome = prompt("Digite seu nome:")
+const sobrenome = prompt(nome + " " + "digite seu sobrenome")
+const idade = prompt(nome + " " + sobrenome + " " + "Você tem quantos anos?")
+const nacionalidade = prompt("Digite sua nacionalidade:")
+
+alert(nome + " " + sobrenome + "\n"+
+idade + "\n"+
+nacionalidade +"\n" +
+"\n Agradecemos por preencher o formulário"
+
+)
