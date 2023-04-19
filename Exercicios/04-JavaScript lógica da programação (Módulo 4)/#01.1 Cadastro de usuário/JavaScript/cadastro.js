9bbe79525592235eb2a9a36d54aff5e37221d6a8
Lucas Ferreira dos Santos <123632958+LucasSantos51@users.noreply.github.com>
2023-04-19 19:15:09 -0300

    cadastroBasico.js

---
+++ Exercicios/04-JavaScript lógica da programação (Módulo 4)/#01.1 Cadastro de usuário/JavaScript/cadastro.js
@@ -0,0 +1,26 @@
+
+window.alert("Seja bem-vindo a inscrição para ser um astronauta")
+
+// Perguntas
+const nome = window.prompt("Digite seu nome:")
+const sobreNome = window.prompt ("Digite seu Sobrenome:")
+const motivo = window.prompt("Porque você gostaria de ser um (a) astronauta?")
+const sobreVoce = window.prompt("Fale-me um pouco sobre você:")
+
+
+// Informações no console
+console.log(nome)
+console.log(dataDeNascimento)
+console.log(motivo)
+console.log(sobreVoce)
+
+
+// Informações finais
+
+window.alert(
+  `Suas informações:\n 
+  Seu nome completo ${nome} ${sobreNome}\n
+  Motivo pelo qual gostaria de ser um astronalta ${motivo}\n
+  Sobre você ${sobreVoce} \n`
+  
+)
