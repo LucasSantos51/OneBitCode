0f134a64ab0f65ca52f68c66c3c0998d8f2192a7
Lucas Ferreira dos Santos <123632958+LucasSantos51@users.noreply.github.com>
2023-05-01 10:41:12 -0300

    Objetos JS

---
+++ aulas_praticas/JavaScript/#1 Lógica da programação/#012 Objetos/objetos.js
@@ -0,0 +1,19 @@
+let identificação = {
+  nome: 'Lucas',
+  idade: 17,
+  Nascimento: "32/12/1919",
+  automoveis: {
+    uno: {
+      cor: "Verde",
+      placa: "btn123",
+      modelo: 2013
+    },
+    fan: {
+      cor: "Azul",
+      placa: "ntb123",
+      modelo: 2015
+    },
+  }
+}
+
+console.log(identificação.automoveis.uno)
