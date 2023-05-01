4265e3983b630f7281b86d502d099a9628be038d
Lucas Ferreira dos Santos <123632958+LucasSantos51@users.noreply.github.com>
2023-05-01 09:06:07 -0300

    Array bidimensionais

---
+++ aulas_praticas/JavaScript/#1 Lógica da programação/#011Array bidimensionais/array.js
@@ -0,0 +1,12 @@
+let lista =["Fejão", "arroz", "macarrão",
+["Tomate", "cebola", "cenoura"],
+  ["2º nível", 42, true],
+  [
+    ["3º nível, 1º item", "Olá, mundo!"],
+    ["3º nível, 2º item", "Oi, mundo!"],
+  ],
+  []
+]
+
+
+console.log(lista)
